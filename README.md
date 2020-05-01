40f6788be5beb87c8c3866961ed5765b52acae17
jero98772 <jero98772@gmail.com>
2020-05-01 01:11:30 -0500

    update readme add todo list and and update code

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # PHpythoncalcultator
-corre en la terminal python ph.py escoje para calcular 'pH' 'pOH' 'H+' o 'OH-' dentro del mismo prorgama  en español
+corre en la terminal python ph.py escoje para calcular 'pH' 'pOH' 'H+' o 'OH-' dentro del mismo prorgama  en español o la ecuacion de henderson hasselbalch hay un programa con interfas web [calcuPH](https://github.com/jero98772/calcupH_js)
+# que calculala
+
+el ph 
+
+el poh
+
+la catidad de hidrogeno
+
+la catidad de oh
+
+soluciones buffer con  henderson hasselbalch
+
+muestra si es ka o kb(constaes de acides y base para de ph mayor que 7)) por los datos que ingresa el usuario
+
+## te mostrara el proseso
+
+#como usarlo
+
+con el comando 
+
+     python3 python ph_calculator/ph.py      
+y te ira pidiendo datos si es ka o kb (constaes de acides y base)
+
+si es ka se va a calcular H y PH henderson hasselbalch "lo menor 7 en el ph"
+
+si es kb se va a calcular oH y Poh "lo mayor 7 en el ph"
+
+## es un programa para calcular PH a 25°C
+#### contibuye con donaciones 
+	
+	- Bitcoin: tb1qgt4trnnsxdmfcpnydkmk8ht0y4esg4cdvzmhaz
+
+o hacer algo de la lista de TODO 
--- ph_calculator/ph.py
+++ ph_calculator/ph.py
@@ -2,12 +2,9 @@
 from math import log ,sqrt,log10
 print ('valor a entar es  kb o ka')
 escoje_kb_o_ka =str(input())
-
-
+#1.8*10**-5
 Moles=eval(input("entra los moles    \n"))
 kb_o_ka =eval(input("k sub_b o k sub_a  \n exponete es '**' \n  "))
-print("escoje para calcular 'pH' 'pOH' 'H+' o 'OH-' o metodo henderson_hasselbalch 'ph2'")
-entrada =str(input())
 ph2 ="pH2"
 ph ="pH"
 poh ="pOH"
@@ -23,14 +20,15 @@ def henderson_hasselbalch(a,b,c):#sal,Moles,kb_o_ka
 	else:
 		paso_dos=14+paso_uno
 		print(paso_dos,"=","14+",paso_uno)
-	if 14>paso_uno:
+	if paso_uno<14:
 		paso_dos=14-paso_uno
-		print("14-",paso_dos,"=",paso_uno)
+		print(paso_dos,"=","14+",paso_uno)
 	else:
 		paso_dos=14+paso_uno
-		print("14+",paso_dos,"=",paso_uno)
+		print(paso_dos,"=","14+",paso_uno)
+"""
 def despejar(a,b,c):
-	disc=round(b*b-4*a*c,50)
+	disc=round((b**2)-4*a*c,50)
 	if(a!=0):
 	 if(disc<0):
 	  print("tiene raices imaginarias")
@@ -38,14 +36,20 @@ def despejar(a,b,c):
 
 	  x1=round(-b+(sqrt(disc))/(2*a),5)
 	  x2=round(-b-(sqrt(disc))/(2*a),5)
-
 	  print("X1 = "+str(x1)+" X2 = "+str(x2))
+	  return x1
+
 	else:
 	 print("coefiente cuadratico debe ser diferente de cero")
-
+"""
+despejar=lambda a, b, c: ((-b + ((b * b) - (4 * a * c))**1/2) / (2 * a), (-b - (((b * b) - (4 * a * c)))**(1/2)) / (2 * a))
 if escoje_kb_o_ka.lower()=="kb":
+	print("escoje para calcular 'pOH'  o 'OH-' ")
+	entrada =str(input())
+
 	if entrada.lower() ==poh.lower():
-		paso_uno=Moles*kb_o_ka				
+
+		paso_uno=despejar(Moles,kb_o_ka,0)				
 		print('"despejamos "',paso_uno)
 		paso_dos=sqrt(paso_uno)
 		print("raiz de",paso_dos)
@@ -77,21 +81,50 @@ if escoje_kb_o_ka.lower()=="kb":
 				print("10elvado",paso_dos,"=",paso_cinco)  
 
 if escoje_kb_o_ka.lower()=="ka":
+	print("escoje para calcular 'pH' 'H+'  o metodo henderson_hasselbalch 'ph2'")
+	entrada =str(input())
+
 	print('es un acido') 
 	if entrada.lower() ==ph.lower():
-		paso_uno=Moles*kb_o_ka				
+		
+		paso_uno=Moles*kb_o_ka
 		print('"despejamos "',paso_uno)
 		paso_dos=sqrt(paso_uno)
-		print("raiz de",paso_dos)
-		print('"despejamos "',paso_uno)
+		print("raiz de",paso_uno,"=",paso_dos)
 		paso_tres=-log10(paso_dos)
 		print("log",paso_tres)
 		if paso_tres>0:
 			paso_cuarto=14-paso_tres
 			print("14-",paso_tres,"=",paso_cuarto)
+			if paso_cuarto>14:
+				paso_uno=Moles/kb_o_ka
+				print('"despejamos "',paso_uno)
+				paso_dos=sqrt(paso_uno)
+				print("raiz de",paso_uno,"=",paso_dos)
+				paso_tres=-log10(paso_dos)
+				print("log",paso_tres)
+				if paso_tres>0:
+					paso_cuarto=14-paso_tres
+					print("14-",paso_tres,"=",paso_cuarto)
+				else:
+					paso_cuarto=14+paso_tres
+					print("14+",paso_tres,"=",paso_cuarto)
 		else:
 			paso_cuarto=14+paso_tres
 			print("14+",paso_tres,"=",paso_cuarto)
+			if paso_cuarto>14:
+				paso_uno=Moles/kb_o_ka
+				print('"despejamos "',paso_uno)
+				paso_dos=sqrt(paso_uno)
+				print("raiz de",paso_uno)
+				paso_tres=-log10(paso_dos)
+				print("log",paso_tres)
+				if paso_tres>0:
+					paso_cuarto=14-paso_tres
+					print("14-",paso_tres,"=",paso_cuarto)
+				else:
+					paso_cuarto=14+paso_tres
+					print("14+",paso_tres,"=",paso_cuarto)
 	if entrada.lower() == ph2.lower():
 		sal =eval(input("entar las sal\n "))		
 		henderson_hasselbalch(Moles,sal,kb_o_ka)
@@ -106,14 +139,13 @@ if escoje_kb_o_ka.lower()=="ka":
 		if paso_tres>0:
 			paso_cuatro=14-paso_tres
 			print("14-",paso_tres,"=",paso_cuatro)
-			if paso_dos> 0:
-				paso_cinco=10**paso_dos
+			if paso_cuatro> 0:
+				paso_cinco=10**-paso_cuatro
 				print("10elvado",paso_dos,"=",paso_cinco)
 		else:
 					
 			paso_cuatro=14+paso_tres
 			print("14+",paso_tres,"=",paso_cuatro)
 			if paso_dos< 0:		
-				paso_cinco=-10**paso_dos
-				print("10elvado",paso_dos,"=",paso_cinco)
-
+				paso_cuarto=-10**paso_cuarto
+				print("10elvado",paso_cuarto,"=",paso_cinco)
